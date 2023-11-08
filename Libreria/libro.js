bd4c0f5cc27994bdf6096096e93c250e00eb8d68
Silvia Mesa Cofrades <120189826+silviamescof@users.noreply.github.com>
2023-11-08 16:32:12 +0100

    16:32

---
+++ Libreria/libro.js
@@ -0,0 +1,51 @@
+class Libro{
+    constructor(titulo,autor,precio,genero,stock){
+
+        this.ISBN=0;
+        this.titulo=titulo;
+        this.autor=autor;
+        this.precio=precio<0 ? 0 : precio;
+        this.genero=genero;
+        this.stock=stock<0 ? 0: stock;
+        
+    };
+
+    ///////////////GETTERS///////////
+    tieneStock(){
+        
+        return this.stock>0 ? true : false;
+    }
+    getTituo(){
+        return this.ISBN;
+    }
+    getAutor(){
+        return this.ISBN;
+    }
+    getPrecio(){
+        return this.ISBN;
+    }
+    getGenero(){
+        return this.ISBN;
+    }
+    getStock(){
+        return this.ISBN;
+    }
+    
+//////////////////SETTERSS////////////////////////
+    setTitulo(titulo){
+        this.titulo=titulo;
+    };
+    setPrecio(precio){
+        this.precio= precio<0 ? 0 : precio;
+    };
+    setAutor(autor){
+        this.autor=autor;
+    };
+    setGenero(genero){
+        this.genero=genero;
+    };
+    setStock(stock){
+        this.stock=stock<0 ? 0: stock;
+    }
+
+}
